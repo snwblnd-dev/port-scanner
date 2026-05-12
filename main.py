53ed63d839a69dae4b01603735ad1902b4edc004
Alex Alderman <74474587+snwblnd-dev@users.noreply.github.com>
2026-05-12 12:25:23 -0500

    refactored code with asyncio library, still have speed issues with blocking functions. Next big piece is speeding the code up.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+
 from port_scan import PortScanner
 
 
 def main():
-    scanner = PortScanner('scanme.nmap.org')
-    scanner.scan_ports()
+
+    scanner = PortScanner('scanme.nmap.org', 1024)
+    # futures = [executor.submit(scanner.scan_ports(scanner.port_range))]
+    # executor.map(scanner.scan_ports, scanner.port_range)
+    asyncio.run(scanner.scan_ports())
+
 
 
 if __name__ == '__main__':
--- port_scan.py
+++ port_scan.py
@@ -1,10 +1,14 @@
+import asyncio
 import socket
 
+
+
 class PortScanner:
-    def __init__(self, hostname):
+    def __init__(self, hostname, max_port = 1024, semaphore = 500):
         self.hostname = hostname
         self.ip = self.establish_connection(hostname)
-
+        self.port_range = range(1, max_port+1)
+        self.semaphore = asyncio.Semaphore(semaphore)
 
 
     def establish_connection(self, hostname):
@@ -15,14 +19,28 @@ class PortScanner:
         except socket.error as e:
             raise ConnectionError(f"Socket Error Occurred: {e}")
 
+    async def connect_socket(self, port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setblocking(False)
+            s.settimeout(.1)
+            return s.connect_ex((self.hostname, port))
 
-    def scan_ports(self, start_port = 1, end_port = 1024):
-        for i in range(start_port, end_port):
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.settimeout(0.1)
-                result = s.connect_ex((self.ip, i))
-                if result == 0:
-                    print('port '+ str(i) + ' is open')
-        return 'Scan complete'
+
+
+
+    async def scan_ports(self):
+
+        tasks = []
+        async with asyncio.TaskGroup() as tg:
+            for i in self.port_range:
+                port_status = tg.create_task(self.connect_socket(i))
+                tasks.append(port_status)
+
+
+        for task in tasks:
+            print(task)
+
+
+
 
 
